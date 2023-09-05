851379e8d7a755266941767fb8dbdaf49291b61b
unknown <judyvicm1@gmail.com>
2023-09-05 23:52:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,22 +3,30 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+  <h1>Contact Us</h1>
+  <form target="_blank" action="https://formsubmit.co/judyvicm1@gmail.com" method="POST">
+    <div class="form-group">
+      <div class="form-row">
+        <div class="col">
+          <input 
+          type="text" 
+          name="name" 
+          class="form-control" 
+          placeholder="Full Name" required/>
+        </div>
+        <div class="col2">
+          <input type="email" name="email" class="form-control" placeholder="Email Address" required/>
+       
     </div>
+    <div class="message">
+      <textarea placeholder="Your Message" class="form-control" name="message" rows="10" required></textarea>
+    </div>
+    <button type="submit" class="btn btn-lg btn-dark btn-block">Submit Form</button>
+ </div>
+      </div>
+  </form>
+</div>
   );
 }
 
